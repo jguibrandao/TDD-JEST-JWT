01dea666cb9bccf143e92fed4c8382860eb71255
jguibrandao <jguibrandao@gmail.com>
2022-02-28 17:35:28 -0300

    setting postgres db

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
+
 module.exports = {
   host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'nodeauth',
+  username: 'postgres',
+  password: process.env.DB_PASSWORD,
+  database: 'TDD',
   dialect: 'postgres',
   operatorAliases: false,
   logging: false,
