0d6fc793ec1dd25d5c9ec93defa7eaab7a222c4c
jguibrandao <jguibrandao@gmail.com>
2022-02-28 17:38:04 -0300

    setting user Model

---
+++ src/app/models/User.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password_hash: DataTypes.STRING
+    })
+
+    return User
+}
